f7255c388c6a060f70d267c4ce5a9584494fa88f
Abolfazl Karbakhsh <110627911+AbolfazlKarbakhsh@users.noreply.github.com>
2023-07-04 11:08:32 +0330

    New Cods

--- htmlCssTamrin/bootstrapProject/js/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load',()=>{
-    setInterval(()=>{
-        const tag = document.querySelector(".preloader");
-        tag.classList.add("hide");
-        document.querySelector("body").style.cssText = " overflow-y: scroll;"
-    },100)
- 
-   
-})
